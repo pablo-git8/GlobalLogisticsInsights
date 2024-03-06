37808be43541dc8f4921070077e9f3a35bf8750d
Pablo Ruiz Lopez <pabloweb8@gmail.com>
2024-03-06 17:28:15 -0600

    DEV: Adding features and interactivity, UX/UI integration completed for the maritime website 1

--- news_app/app.py
+++ news_app/app.py
@@ -1,7 +1,11 @@
 # cd to the app project
 # python -m streamlit run app.py
 
+import sqlite3
 import streamlit as st
+
+from datetime import datetime
+
 
 ######## CSS ########
 
@@ -13,34 +17,137 @@ with open('./css/styles.css', 'r') as f:
 st.markdown(f'<style>{css_to_include}</style>', unsafe_allow_html=True)
 
 
-######## APP PRESENTATION ########
+######## APP INITIALIZATION ########
 
 # App title
 st.title('Maritime & Air News Gatherer')
 
+# App Image
 with st.container():
     col1, col2, col3 = st.columns([2, 4, 2])
     # Display an image with a specified width, maintaining aspect ratio
     with col2:
         st.image('https://raw.githubusercontent.com/pablo-git8/GlobalLogisticsInsights/main/images/mar_air_transport.png', width=300)
 
+# Initialize session state variable if it doesn't exist
+if 'show_text' not in st.session_state:
+    st.session_state.show_text = True 
+
+# Define a callback to hide the text
+def hide_text():
+    st.session_state.show_text = False
+
+# Define the path to the SQLite database
+db_path = '../data/news/maritime_news.db'
+
+# Define table naming by date of execution
+current_date = datetime.now().strftime("%m%d%Y")
+
+# Function to get news from the database based on the selected category
+def get_news_data(option, classification, num_news):
+    """
+    """
+    conn = sqlite3.connect(db_path)  # Update with your SQLite DB path
+    cur = conn.cursor()
+
+    # Determine the table name based on the option
+    table_suffix = "air" if option == '✈️ Air News' else "mar"
+    table_name = f"{table_suffix}_news_{current_date}"
+
+    # Count the total news in the category
+    cur.execute(f"SELECT COUNT(*) FROM {table_name} WHERE classification = ?", (classification,))
+    count = cur.fetchone()[0]
+
+    # Maximum number of news to select
+    limit = num_news if count >= num_news else count
+
+    # Then, fetch the limited number of news articles
+    cur.execute(f"""
+        SELECT title, text, summary, link
+        FROM {table_name }
+        WHERE classification = ? 
+        LIMIT ?
+    """, (classification, limit))
+
+    # Get data
+    data = cur.fetchall()
+    conn.close()
+
+    return data
+
+
+######## CHOOSE NEWS SECTION (SIDEBAR) ########
+        
+# Sidebar Title
+st.sidebar.title('News of Interest')
+
+# Drop-down menu for choosing news transport
+news_option = st.sidebar.radio(
+    'Transport:',
+    ('🚢 Maritime News', '✈️ Air News'))
+
+# Drop-down menu for choosing news location
+news_loc = st.sidebar.radio(
+    'Location:',
+    ('🌐 Global', '🌎 South America'))
+
+# Drop-down menu for choosing news categories
+news_cat = st.sidebar.selectbox(
+    'Category:',
+    ('General', 'Regulations', 'Issues', 'Global Trade', 'Port Operations',
+     'Shipping Market', 'Supply Chain', 'Customs', 'Regulations'))
+
+# Select number of news to display
+num_news = st.sidebar.slider('Number of News:', min_value=1, max_value=10, value=1)
+
+
+######## NEWS OF INTEREST ########
+
+# App section
+st.markdown('<p class="centered-subheader">Selected News</p>', unsafe_allow_html=True)
+# Insert custom HTML to create a horizontal line separator
+st.markdown('<hr style="border:1px solid red">', unsafe_allow_html=True)
+
+# Display the text if the corresponding state is True
+if st.session_state.show_text:
+    st.markdown("<h4 style='margin-bottom:0px; color: #FFFFCC;'>👈 Select your news of interest...</h4>", unsafe_allow_html=True)
+
+
+#### BUTTON DISPLAY NEWS! ####
+
+# Button to trigger the display of the text
+if st.sidebar.button('Display News!', on_click=hide_text):
+    news_items = get_news_data(news_option, news_cat, num_news)
+    if len(news_items) == 0:
+        # Display a message if no news items were found
+        st.markdown('<p class="notification">No news found today!</p>', unsafe_allow_html=True)
+    else:
+        for news_title, news_content, summary, link in news_items:
+            # Container of news
+            with st.container():
+                col1, col2 = st.columns([7, 5])
+                with col1:
+                    # Make the news title a clickable link
+                    st.markdown(f'<p class="subheader-font"><a href="{link}" target="_blank">{news_title}</a></p>', unsafe_allow_html=True)
+                    with st.expander("Read More"):
+                        st.markdown(f'<div class="quote">{news_content[:1300]}...</div>', unsafe_allow_html=True)
+                with col2:
+                    st.markdown(f'<div class="caption-code">🤖 {summary}</div>', unsafe_allow_html=True)
+
 
 ######## URGET NEWS SECTION (5 NEWS) ########
-        
-# Insert custom HTML to create a horizontal line separator
-st.markdown('<hr style="border:0.1px black">', unsafe_allow_html=True)
-# App section
-#st.subheader('🚨 Urgent Issues in Maritime / Air Transport 🚨')
-st.markdown('<p class="centered-subheader">🚨 Urgent Issues in Maritime / Air Transport 🚨</p>', unsafe_allow_html=True)
-# Insert custom HTML to create a horizontal line separator
-st.markdown('<hr style="border:1px solid #D4FFB2">', unsafe_allow_html=True)
 
-# TODO - Get 5 news tagged as issues, regulations, etc.
+# App section
+st.markdown('<p class="centered-subheader">AI Recommended News</p>', unsafe_allow_html=True)
+# Insert custom HTML to create a horizontal line separator
+st.markdown('<hr style="border:1px solid red">', unsafe_allow_html=True)
 
 # Container of news 1
 with st.container():
+    # Risks
+    st.markdown('<p class="subheader-cat">Risks</p>', unsafe_allow_html=True)
     # Assign custom CSS class to the container
-    col1, col2 = st.columns([6, 6])
+    col1, col2 = st.columns([7, 5])
     with col1:
         # Header for the news title
         st.markdown('<p class="subheader-font">Tensions in the Red Sea</p>', unsafe_allow_html=True)
@@ -49,101 +156,31 @@ with st.container():
             st.markdown('<div class="quote">Display regulation news...</div>', unsafe_allow_html=True)
     with col2:
         # Summary of the news title
-        st.markdown('<div class="caption-code">Get Premium for enabling AI-powered summary!</div>', unsafe_allow_html=True)
+        st.markdown('<div class="caption-code">🤖 Get Premium for AI-powered summary!</div>', unsafe_allow_html=True)
 
 # Container of news 2
 with st.container():
-    col1, col2 = st.columns([6, 6])
+    # Oportunities
+    st.markdown('<p class="subheader-cat">Oportunities</p>', unsafe_allow_html=True)
+    col1, col2 = st.columns([7, 5])
     with col1:
         st.markdown('<p class="subheader-font">Tensions in the Red Sea</p>', unsafe_allow_html=True)
         with st.expander("Read More"):
             st.markdown('<div class="quote">Display regulation news...</div>', unsafe_allow_html=True)
     with col2:
-        st.markdown('<div class="caption-code">Get Premium for enabling AI-powered summary!</div>', unsafe_allow_html=True)
+        st.markdown('<div class="caption-code">🤖 Get Premium for AI-powered summary!</div>', unsafe_allow_html=True)
 
 # Container of news 3
 with st.container():
-    col1, col2 = st.columns([6, 6])
+    # General
+    st.markdown('<p class="subheader-cat">General</p>', unsafe_allow_html=True)
+    col1, col2 = st.columns([7, 5])
     with col1:
         st.markdown('<p class="subheader-font">Tensions in the Red Sea</p>', unsafe_allow_html=True)
         with st.expander("Read More"):
             st.markdown('<div class="quote">Display regulation news...</div>', unsafe_allow_html=True)
     with col2:
-        st.markdown('<div class="caption-code">Get Premium for enabling AI-powered summary!</div>', unsafe_allow_html=True)
-
-# Container of news 4
-with st.container():
-    col1, col2 = st.columns([6, 6])
-    with col1:
-        st.markdown('<p class="subheader-font">Tensions in the Red Sea</p>', unsafe_allow_html=True)
-        with st.expander("Read More"):
-            st.markdown('<div class="quote">Display regulation news...</div>', unsafe_allow_html=True)
-    with col2:
-        st.markdown('<div class="caption-code">Get Premium for enabling AI-powered summary!</div>', unsafe_allow_html=True)
-
-# Container of news 5
-with st.container():
-    col1, col2 = st.columns([6, 6])
-    with col1:
-        st.markdown('<p class="subheader-font">Tensions in the Red Sea</p>', unsafe_allow_html=True)
-        with st.expander("Read More"):
-            st.markdown('<div class="quote">Display regulation news...</div>', unsafe_allow_html=True)
-    with col2:
-        st.markdown('<div class="caption-code">Get Premium for enabling AI-powered summary!</div>', unsafe_allow_html=True)
-
-
-######## CHOOSE NEWS SECTION ########
-        
-# Insert custom HTML to create a horizontal line separator
-st.markdown('<hr style="border:0.1px black">', unsafe_allow_html=True)
-# App section
-st.subheader('Choose News of Interest')
-# Insert custom HTML to create a horizontal line separator
-st.markdown('<hr style="border:0.5px solid #D4FFB2">', unsafe_allow_html=True)
-
-# Drop-down menu for choosing news transport
-news_option = st.selectbox(
-    'Choose transport:',
-    ('🚢 Maritime News', '✈️ Air News'))
-
-# Drop-down menu for choosing news category
-news_cat = st.selectbox(
-    'Choose category:',
-    ('🌐 Global', '🌎 South America'))
-
-# Button to trigger the display of the text
-if st.button('Display News!'):
-
-    # Maritime news
-    if news_option == 'Maritime News':
-        # Global maritime news
-        if news_cat == 'Global':
-            # Container of news
-            with st.container():
-                # Header for the news title
-                st.subheader('Global Maritime News Title')
-                # Use an expander for displaying the news content
-                with st.expander("View Global Maritime News"):
-                    st.write('Displaying Global Maritime News...')
-                # Summary of the news title
-                st.caption('Global Maritime News Title Summary')
-
-        # South America maritime news
-        if news_cat == 'South America':
-            st.write('South America Maritime News Title')
-            st.write('Displaying South America Maritime News...')
-            st.write('South America Maritime News Title Summary')
-
-    # Air news
-    elif news_option == 'Air News':
-        if news_cat == 'Global':
-            st.write('Global Air News Title')
-            st.write('Displaying Global Air News...')
-            st.write('Air Maritime News Title Summary')
-        if news_cat == 'South America':
-            st.write('South America Air News Title')
-            st.write('Displaying South America Air News...')
-            st.write('South America Air News Title Summary')
+        st.markdown('<div class="caption-code">🤖 Get Premium for AI-powered summary!</div>', unsafe_allow_html=True)
 
 
 ######## PREMIUM EXAMPLE SECTION #########
@@ -178,10 +215,12 @@ summary_example = \
 # Container of news
 with st.container():
     # Assign custom CSS class to the container
-    col1, col2 = st.columns([6, 6])
+    col1, col2 = st.columns([7, 5])
     with col1:
+        # Linl to project
+        link_project = 'https://github.com/pablo-git8/GlobalLogisticsInsights'
         # Header for the news title
-        st.markdown('<p class="subheader-font">Tensions in the Red Sea</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="subheader-font"><a href="{link_project}" target="_blank">Tensions in the Red Sea</a></p>', unsafe_allow_html=True)
         # Use an expander for displaying the news content
         with st.expander("Read More"):
             st.markdown(f'<div class="quote">{text_example}</div>', unsafe_allow_html=True)
@@ -190,3 +229,11 @@ with st.container():
         st.markdown(f'<div class="quote-test">🤖 AI-Powered Summary:</div>', unsafe_allow_html=True)
         st.write(f'')
         st.write(f'{summary_example}')
+
+# Add a copyright notice at the bottom of the page
+st.markdown("""
+    <hr style="border:0.5px dash #f0f0f5;">
+    <p style="text-align: center; color: grey;">
+        &copy; 2024 Pablo-git8. All rights reserved.
+    </p>
+    """, unsafe_allow_html=True)
